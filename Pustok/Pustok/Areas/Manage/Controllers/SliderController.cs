625fdda4930c5522e7346df1e02d74428afd7686
Elchin Aghayev <Elchin@code.local>
2023-06-19 17:41:49 +0400

    project adde

---
+++ Pustok/Pustok/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.Areas.Manage.ViewModels;
+using Pustok.DAL;
+using Pustok.Helpers;
+using Pustok.Models;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SliderController : Controller
+    {
+        private readonly PustokDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public SliderController(PustokDbContext context,IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index(int page=1)
+        {
+            var query = _context.Sliders.AsQueryable();
+            return View(PaginatedList<Slider>.Create(query,page,2));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Slider slider)
+        {
+            if (!ModelState.IsValid) return View();
+
+            if (slider.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "ImageFile is required");
+                return View();
+            }
+
+            if (slider.ImageFile.Length > 2 * 1024 * 1024)
+            {
+                ModelState.AddModelError("ImageFile", "ImageFile max size is 2MB");
+                return View();
+            }
+
+            if(slider.ImageFile.ContentType!="image/jpeg" && slider.ImageFile.ContentType != "image/png")
+            {
+                ModelState.AddModelError("ImageFile", "ImageFile must be .jpg,.jpeg or .png");
+                return View();
+            }
+          
+            slider.ImageName = FileManager.Save(slider.ImageFile,_env.WebRootPath,"manage/uploads/sliders");
+
+            _context.Sliders.Add(slider);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+    }
+}
